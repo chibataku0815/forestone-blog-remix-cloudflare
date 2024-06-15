85c5f839425973c9fc0da77c64ef8c58e9baf405
chibataku0815 <chiba@fores-tone.co.jp>
2024-06-15 14:09:53 +0900

    chore: Add @mdx-js/rollup and update vite.config.ts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,57 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { json, type MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/.server/posts";
+import { Post } from "~/components/post";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix on Cloudflare!",
-    },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{
+			name: "description",
+			content: "Welcome to Remix on Cloudflare!",
+		},
+	];
+};
+
+export const loader = async () => {
+	const posts = await getPosts();
+	return json(posts.filter((post) => post.frontmatter.featured));
 };
 
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const featuredPosts = useLoaderData<typeof loader>();
+
+	return (
+		<div className="flex-1 p-10 grid sm:grid-cols-2 gap-16 sm:place-items-center">
+			<div className="space-y-8">
+				<div>
+					<h2 className="text-4xl font-bold">Remix 🤝 MDX</h2>
+					<p className="text-gray-600 font-light">
+						Powered by Vite plugins. Check out the{" "}
+						<a href="https://github.com/pcattori/remix-blog-mdx">
+							code on Github
+						</a>
+						.
+					</p>
+				</div>
+				<hr />
+				<section>
+					<h3 className="text-xl tracking-wide">✨ FEATURED ✨</h3>
+					<ul className="mt-4 space-y-8">
+						{featuredPosts.map((post) => (
+							<li key={post.slug}>
+								<Post {...post} />
+							</li>
+						))}
+					</ul>
+				</section>
+			</div>
+			<div className="hidden sm:block">
+				<img
+					src="/hero.png"
+					alt="Abstract sculpture with different colorful shapes"
+				/>
+			</div>
+		</div>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,20 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from "@mdx-js/rollup";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import rehypePrettyCode from "rehype-pretty-code";
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
+    mdx({
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [
+        rehypePrettyCode,
+      ],
+    }),
     remixCloudflareDevProxy(),
     remix({
       future: {
@@ -15,6 +26,5 @@ export default defineConfig({
         v3_throwAbortReason: true,
       },
     }),
-    tsconfigPaths(),
   ],
 });
